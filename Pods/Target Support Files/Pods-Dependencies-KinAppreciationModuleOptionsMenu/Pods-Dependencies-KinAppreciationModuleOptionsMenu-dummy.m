d5f0d4f987f8492dbe10f63e51d943291069b540
Corey Werner <cnotethegr8@gmail.com>
2019-06-17 13:12:52 +0300

    removed pods from git

--- Pods/Target Support Files/Pods-Dependencies-KinAppreciationModuleOptionsMenu/Pods-Dependencies-KinAppreciationModuleOptionsMenu-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Dependencies_KinAppreciationModuleOptionsMenu : NSObject
-@end
-@implementation PodsDummy_Pods_Dependencies_KinAppreciationModuleOptionsMenu
-@end
--- Pods/Target Support Files/Pods-Dependencies-KinAppreciationModuleOptionsMenuSampleApp/Pods-Dependencies-KinAppreciationModuleOptionsMenuSampleApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Dependencies_KinAppreciationModuleOptionsMenuSampleApp : NSObject
-@end
-@implementation PodsDummy_Pods_Dependencies_KinAppreciationModuleOptionsMenuSampleApp
-@end
